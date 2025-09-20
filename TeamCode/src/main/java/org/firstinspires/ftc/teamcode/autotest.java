9c1cbb586b63bc4082830c75be3418aa22b44989
ilarao <ilakaurrao@gmail.com>
2025-09-19 20:21:18 -0700

    Update autotest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autotest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autotest.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class autotest extends LinearOpMode {
@@ -9,6 +12,25 @@ public class autotest extends LinearOpMode {
         MecanumDrive drive = new MecanumDrive(hardwareMap,new Pose2d(0, 0, 0));
 
         waitForStart();
+
+        Action driveAction = drive.actionBuilder(new Pose2d(0,0,0));
+        .splineTo(new Vector2d(-48, 24), Math.toRadians(225))
+                .waitSeconds(0.5)
+                .splineTo(new Vector2d(-12, 38), Math.toRadians(90))
+                .waitSeconds(1)
+                .splineTo(new Vector2d(-24,24), Math.toRadians(135))
+                .waitSeconds(1)
+                .splineTo(new Vector2d(12, 38), Math.toRadians(90))
+                .waitSeconds(1)
+                .splineTo(new Vector2d(-24, 24), Math.toRadians(135))
+                .waitSeconds(1)
+                .splineTo(new Vector2d(38,38), Math.toRadians(90))
+                .waitSeconds(1)
+                .splineTo(new Vector2d(-24,24), Math.toRadians(135))
+                .build
+
+        Actions.runBlocking(driveAction);
+
     }
 
 }
