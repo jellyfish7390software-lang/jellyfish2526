4753c159bd8159914c81806bd4b4ae81a3c49c5b
MaximeSobrier <142960631+MaximeSobrier@users.noreply.github.com>
2025-10-08 21:49:37 -0700

    XML Configurations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -225,10 +225,10 @@ public final class MecanumDrive {
 
         // TODO: make sure your config has motors with these names (or change them)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
-        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightBack");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightFront");
 
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -238,6 +238,10 @@ public final class MecanumDrive {
         // TODO: reverse motor directions if needed
 
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
 
         // TODO: make sure your config has an IMU with this name (can be BNO or BHI)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleTest.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.Robot;
+
+@Config
+@TeleOp
+public class TeleTest extends LinearOpMode {
+
+    public static double targetVel = 0;
+
+    public double vel;
+
+    public static double ticksPerRev = 27.0;
+
+    public static double p = 10, i = 0, d = 0, f = 12;
+
+    public static double power = 0.0;
+
+    public static double shooterPower = 0.0;
+
+    public static double diverterPos = 0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Robot bot = new Robot(hardwareMap);
+
+        bot.shooter.setVelocityPIDFCoefficients(p, i, d, f);
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+//            bot.shooter.setVelocityPIDFCoefficients(p, i, d, f);
+//
+//            vel = bot.shooter.getVelocity() / ticksPerRev;
+//
+//            bot.shooter.setVelocity((targetVel / 60.0) * ticksPerRev);
+//
+            bot.leftIntake.setPower(-power);
+            bot.rightIntake.setPower(-power);
+//
+            bot.diverter.setPosition(diverterPos);
+//
+            bot.shooter.setPower(shooterPower);
+
+            telemetry.addData("Velocity: RPM", vel*60);
+            telemetry.addData("Target Velocity", targetVel);
+            telemetry.addData("Ticks", bot.shooter.getCurrentPosition());
+            telemetry.addData("Shooter Power", bot.shooter.getPower());
+            telemetry.update();
+        }
+
+
+    }
+}
