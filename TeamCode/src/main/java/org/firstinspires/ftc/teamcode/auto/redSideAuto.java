da7e0d8684b64992d8fec0b9d5d6c06e591e0715
jadyn-24 <jadynhkao@gmail.com>
2025-11-11 17:39:20 -0800

    Update redSideAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/redSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/redSideAuto.java
@@ -49,28 +49,28 @@ public class redSideAuto extends LinearOpMode {
                 .setReversed(true)
                 .splineToLinearHeading(new Pose2d(-12, 38, Math.toRadians(90)), Math.toRadians(90))
                 .waitSeconds(3)
-                .afterTime(0, bot.shootFull())
+                .afterTime(0, () -> bot.shootFull())
 
                 .setReversed(true)
                 .splineToLinearHeading(new Pose2d(-24, 24, Math.toRadians(135)), Math.toRadians(135))
                 .waitSeconds(3)
-                .afterTime(0, bot.shootFull())
+                .afterTime(0, () -> bot.shootFull())
 
 
                 .setReversed(true)
                 .splineToLinearHeading(new Pose2d(12, 38, Math.toRadians(90)), Math.toRadians(90))
                 .waitSeconds(3)
-                .afterTime(0, bot.shootFull())
+                .afterTime(0, () -> bot.shootFull())
 
                 .setReversed(true)
                 .splineToLinearHeading(new Pose2d(-24, 24, Math.toRadians(135)), Math.toRadians(135))
                 .waitSeconds(3)
-                .afterTime(0, bot.shootFull())
+                .afterTime(0, () -> bot.shootFull())
 
                 .setReversed(true)
                 .splineToLinearHeading(new Pose2d(38, 38, Math.toRadians(90)), Math.toRadians(90))
                 .waitSeconds(3)
-                .afterTime(0, bot.shootFull())
+                .afterTime(0, () -> bot.shootFull())
 
                 .setReversed(true)
                 .splineToLinearHeading(new Pose2d(-24, 24, Math.toRadians(135)), Math.toRadians(135))
