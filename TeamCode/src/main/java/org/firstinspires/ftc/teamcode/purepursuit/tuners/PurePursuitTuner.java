4aa5985a07d56d63de92bc2a69d2fe9982ee6d45
vik-j <114836259+vik-j@users.noreply.github.com>
2025-12-02 21:52:04 -0800

    tuned squid for pink dt, fixing pose not updating

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/purepursuit/tuners/PurePursuitTuner.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.purepursuit.tuners;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class PurePursuitTuner extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
